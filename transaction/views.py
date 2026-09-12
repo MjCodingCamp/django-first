ab40e3222baa66af36b887ef46099dc893e6a001
Mohammad Jeeshan <Mohammad.Jeeshan.91@gmail.com>
2026-09-12 22:45:55 +0530

    [ADD]: get transactions api

--- transaction/views.py
+++ transaction/views.py
@@ -1,8 +1,29 @@
 from rest_framework.decorators import APIView
 from .serializers import TransactionSerializer
 from django.http import JsonResponse
+from .models import Transaction
 
 class TransactionView(APIView):
+
+    def get(self, request):
+        user_id = request.query_params.get('user_id')
+
+        if not user_id:
+            return JsonResponse({
+                'status_code': 404,
+                'message': 'user_id is required',
+                'data': []
+            })
+
+        transactions = Transaction.objects.filter(user_id=user_id)
+        serializer = TransactionSerializer(transactions, many=True)
+
+        return JsonResponse({
+            'status_code': 200,
+            'message': 'Transactions fetched successfully',
+            'data': serializer.data
+        })
+             
 
     def post(self, request):
         data = request.data 
